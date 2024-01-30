812f8b45cda00a402e96890019822088593b7bee
brutalvik <vikcanada90@gmail.com>
2024-01-30 03:14:12 -0700

    Added active tab identifier

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,6 +15,8 @@ import {
   VideoIcon,
 } from "lucide-react";
 
+import { usePathname } from "next/navigation";
+
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
 const routes = [
@@ -62,6 +64,8 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="space-y-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -78,7 +82,12 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font.medium cursor-pointer hover:text-2hite hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font.medium cursor-pointer hover:text-2hite hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 2-5 mr-3", route.color)} />
