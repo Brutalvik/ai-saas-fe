6fa7c99fdf762a5e7e5f1d2a01a2ae6e1494468b
brutalvik <vikcanada90@gmail.com>
2024-02-01 21:10:41 -0700

    Added modal to upgrade

---
+++ components/pro-modal.tsx
@@ -0,0 +1,31 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useProModal } from "@/hooks/use-pro-modal";
+import { Badge } from "@/components/ui/badge";
+
+export const ProModal = () => {
+  const { isOpen, onClose } = useProModal();
+
+  return (
+    <div>
+      <Dialog open={isOpen} onOpenChange={onClose}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+              <div className="flex items-center gap-x-2 font-bold py-1">
+                Upgrade to Genius
+                <Badge className="uppercase text-sm py-1" variant="premium">
+                  pro
+                </Badge>
+              </div>
+            </DialogTitle>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
