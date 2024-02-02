27df1ab39a9beae372860cb9859fc59185db73ec
brutalvik <vikcanada90@gmail.com>
2024-02-01 21:23:40 -0700

    Added upgrade request modal

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,11 +1,26 @@
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import {
+  Check,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon,
+  Zap,
+} from "lucide-react";
 import { useProModal } from "@/hooks/use-pro-modal";
 import { Badge } from "@/components/ui/badge";
+import { tools } from "@/constants";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 export const ProModal = () => {
   const { isOpen, onClose } = useProModal();
@@ -23,7 +38,31 @@ export const ProModal = () => {
                 </Badge>
               </div>
             </DialogTitle>
+            <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
+              {tools.map((tool) => (
+                <Card
+                  key={tool.label}
+                  className="p-3 border-black/5 flex items-center justify-between"
+                >
+                  <div className="flex items-center gap-x-4">
+                    <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                      <tool.icon
+                        className={cn("w-6 h-6", tool.color)}
+                      ></tool.icon>
+                    </div>
+                    <div className="font-semibold text-sm">{tool.label}</div>
+                  </div>
+                  <Check className="text-primary w-5 h-5" />
+                </Card>
+              ))}
+            </DialogDescription>
           </DialogHeader>
+          <DialogFooter>
+            <Button size="lg" variant="premium" className="w-full">
+              Upgrade
+              <Zap className="w-4 h-4 ml-2 fill-white" />
+            </Button>
+          </DialogFooter>
         </DialogContent>
       </Dialog>
     </div>
