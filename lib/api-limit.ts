aa689b98e91400e96d329aa389080604ca2b04f9
brutalvik <vikcanada90@gmail.com>
2024-02-01 20:13:42 -0700

    Added API limits

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -9,40 +9,38 @@ export const increaseApiLimit = async () => {
   }
   const userApiLimit = await prismadb.userApiLimit.findUnique({
     where: {
-      userId
-    }
-  })
+      userId,
+    },
+  });
 
-  if(userApiLimit) {
-    await prismadb.userApiLimit.update){
-      where: {userId: userId},
-      data: { count: userApiLimit.count++ }
-    }
-  }
-  else {
+  if (userApiLimit) {
+    await prismadb.userApiLimit.update({
+      where: { userId: userId },
+      data: { count: userApiLimit.count++ },
+    });
+  } else {
     await prismadb.userApiLimit.create({
-      data: {userId: userId, count: 1}
-    })
+      data: { userId: userId, count: 1 },
+    });
   }
 };
 
 export const checkApiLimit = async () => {
-    const {userId } = auth();
-    
-    if(!userId) {
-      return false;
-    }
+  const { userId } = auth();
 
-    const userApiLimit = await prismadb.userApiLimit.findUnique({
-      where: {
-        userId: userId
-      }
-    })
+  if (!userId) {
+    return false;
+  }
 
-    if(!userApiLimit || userApiLimit.count < MAX_FREE_COUNTS) {
-      return true;
-    }
-    else {
-      return false;
-    }
-}
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: {
+      userId: userId,
+    },
+  });
+
+  if (!userApiLimit || userApiLimit.count < MAX_FREE_COUNTS) {
+    return true;
+  } else {
+    return false;
+  }
+};
